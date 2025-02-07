f458aa54ec7f42d484b91ebf8eac389207b56761
LukasA707 <lukas707@hotmail.com>
2025-02-07 21:49:09 +0100

    Created connect_db and format_datetime functions

--- Program.cs
+++ Program.cs
@@ -10,29 +10,50 @@ int PER_PAGE = 30;
 bool DEBUG = true;
 string SECRET_KEY = "development key";
 
+SqliteConnection connect_db() {
+    /* Returns a new connection to the database. */
+    return new SqliteConnection("Data source=" + DATABASE);
+}
+
+void query_db(string query, bool one=false) {
+    /* Queries the database and returns a list of dictionaries. */
+}
+
+void get_user_id(string username) {
+    /* Convenience method to look up the id for a username. */
+}
+
+string format_datetime(long timestamp) {
+    /* Format a timestamp for display. */
+    DateTimeOffset datetime = DateTimeOffset.FromUnixTimeSeconds(timestamp);
+    return datetime.ToString("yyyy-mm-dd @ hh:mm");
+}
+
+void gravatar_url(string email, int size=80) {
+    /* Return the gravatar image for the given email address. */
+}
+
 app.MapGet("/", () => {
 
-    using (var connection = new SqliteConnection("Data Source=" + DATABASE))
+    SqliteConnection connection = connect_db();
+    
+    connection.Open();
+
+    var command = connection.CreateCommand();
+    command.CommandText =
+    @"
+        SELECT * FROM message
+    ";
+
+    using (var reader = command.ExecuteReader())
     {
-        connection.Open();
-
-        var command = connection.CreateCommand();
-        command.CommandText =
-        @"
-            SELECT * FROM message
-        ";
-
-        using (var reader = command.ExecuteReader())
+        while (reader.Read())
         {
-            while (reader.Read())
-            {
-                var name = reader.GetString(0);
+            var name = reader.GetString(0);
 
-                Console.WriteLine($"Hello, {name}!");
-            }
+            Console.WriteLine($"Hello, {name}!");
         }
     }
-
 });
 
 // Configure the HTTP request pipeline.
