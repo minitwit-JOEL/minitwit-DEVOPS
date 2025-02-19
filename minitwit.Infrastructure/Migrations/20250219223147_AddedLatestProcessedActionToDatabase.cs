c151173e25fb83cb1e67cb2fc7d189c2b8c8ea2f
Oliver Prip <ohag@itu.dk>
2025-02-19 23:57:09 +0100

    Added processedActionId as table in database

---
+++ minitwit.Infrastructure/Migrations/20250219223147_AddedLatestProcessedActionToDatabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace minitwit.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLatestProcessedActionToDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
