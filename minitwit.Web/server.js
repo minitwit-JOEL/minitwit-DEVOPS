f09769e360d56a924f1178929edb80dfa6090263
Jerome Rahal <jrah@itu.dk>
2025-02-19 10:30:42 +0100

    minor improvements for running development mode with docker as backend

--- minitwit.Web/server.js
+++ minitwit.Web/server.js
@@ -24,7 +24,7 @@ app.all('*', createRequestHandler({ build }))
 // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
 app.disable('x-powered-by')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3100
 app.listen(port, '0.0.0.0', () => {
   console.log(`App listening on http://localhost:${port}`)
 })
