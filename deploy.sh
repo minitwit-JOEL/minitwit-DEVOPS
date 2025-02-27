95107d4360366c458582f8ffbe7a91a6411098ed
LukasA707 <lukas707@hotmail.com>
2025-02-28 00:58:43 +0100

    Changed docker run in 'deploy.sh'.
    Correct exposure of ports when running api container with docker.
    Added enviroment variables to when running web container with docker.

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,14 @@ docker container prune -f
 
 docker run -d --network=minitwit-network -p 5432:5432 --name=minitwit-db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=minitwit lukan707/minitwit-joel-db:latest
 
-until [ "$(docker inspect -f '{{.State.Health.Status}}' minitwit-db)" == "healthy" ]; do
-  sleep 1
-  echo 'Database is not ready'
-done
+#until [ "$(docker inspect -f '{{.State.Health.Status}}' minitwit-db)" == "healthy" ]; do
+#  sleep 1
+#  echo 'Database is not ready'
+#done
 
-docker run -d --network=minitwit-network -p 8080:8081 --name=minitwit-api -e "ConnectionStrings__DefaultConnection=Host=minitwit-db;Port=5432;Database=minitwit;Username=postgres;Password=postgres" lukan707/minitwit-joel-api:latest
-docker run -d --network=minitwit-network -p 3000:3000 --name=minitwit-web lukan707/minitwit-joel-web:latest 
+sleep 60
+
+docker run -d --network=minitwit-network -p 8080:8080 -p 8081:8081 --name=minitwit-api -e "ConnectionStrings__DefaultConnection=Host=minitwit-db;Port=5432;Database=minitwit;Username=postgres;Password=postgres" lukan707/minitwit-joel-api:latest
+docker run -d --network=minitwit-network -p 3000:3000 --name=minitwit-web -e "API_BASE_URL=https://localhost:8080/" -e "NODE_TLS_REJECT_UNAUTHORIZED=0" lukan707/minitwit-joel-web:latest 
 
 docker image prune -f
