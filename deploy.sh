fd76ab7beafb87204946d10f119d4c52cf0093a4
LukasA707 <lukas707@hotmail.com>
2025-02-27 13:41:49 +0100

    Not in a working state.

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env  bash
 
-docker image pull :latest
-docker image pull :latest
-docker image pull :latest
+docker image pull lukan707/minitwit-joel-db:latest
+docker image pull lukan707/minitwit-joel-api:latest
+docker image pull lukan707/minitwit-joel-web:latest
 
 docker stop $(docker ps -q)
 
-docker run -d :latest
-docker run -d :latest
-docker run -d :latest
+docker network create minitwit-network
 
 docker container prune -f
-docker image prune
+
+docker run -d --network=minitwit-network -p 5432:5432 --name=minitwit-db -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=minitwit lukan707/minitwit-joel-db:latest
+
+until [ "$(docker inspect -f '{{.State.Health.Status}}' minitwit-db)" == "healthy" ]; do
+  sleep 1
+  echo 'Database is not ready'
+done
+
+docker run -d --network=minitwit-network -p 8080:8081 --name=minitwit-api -e "ConnectionStrings__DefaultConnection=Host=minitwit-db;Port=5432;Database=minitwit;Username=postgres;Password=postgres" lukan707/minitwit-joel-api:latest
+docker run -d --network=minitwit-network -p 3000:3000 --name=minitwit-web lukan707/minitwit-joel-web:latest 
+
+docker image prune -f
