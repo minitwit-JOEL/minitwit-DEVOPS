45adf74487967a31543858722a53979cb2aaaa53
LukasA707 <Lukas707@hotmail.com>
2025-03-27 17:53:29 +0100

    mend

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,6 @@ until  docker exec minitwit-db pg_isready -U postgres; do
  echo 'Database is not ready'
 done
 
-# sleep 60
-
 docker run -d --network=minitwit-network -p 8080:8080 -p 8081:8081 --name=minitwit-api --env-file .secrets lukan707/minitwit-joel-api:latest
 docker run -d --network=minitwit-network -p 3000:3000 --name=minitwit-web -e "API_BASE_URL=http://minitwit-api:8080/" -e "NODE_TLS_REJECT_UNAUTHORIZED=0" lukan707/minitwit-joel-web:latest 
 
