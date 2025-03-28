d77e4e6f97718a9a852da25f1fdeb41473da5d8d
Edward Rostomian <edwr@itu.dk>
2025-03-28 14:29:03 +0100

    Revert "update build/test workflow"
    This reverts commit 4c936a200cb3358c6bc3a03a329e477f86443e93.

--- src/minitwit.Api/Program.cs
+++ src/minitwit.Api/Program.cs
@@ -24,8 +24,7 @@ builder.Services.AddDistributedMemoryCache();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
         options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddSession(options =>
-{
+builder.Services.AddSession(options => {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
     options.Cookie.HttpOnly = false;
     options.Cookie.IsEssential = false;
@@ -64,12 +63,12 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         {
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetValue<string>("Token:Key"))),
-            ValidIssuer = builder.Configuration.GetValue<string>("Token:Issuer"),
+            ValidIssuer = builder.Configuration.GetValue<string>("Token:Issuer"), 
             ValidAudience = builder.Configuration.GetValue<string>("Token:Audience"),
             ValidateIssuer = true,
             ValidateAudience = true,
             ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero
+            ClockSkew = TimeSpan.Zero 
         };
     });
 
@@ -108,5 +107,3 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.Run();
-
-public partial class Program { }
