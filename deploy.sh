c8697a263480df8a7ae836b9fc455c72d2d54bba
LukasA707 <Lukas707@hotmail.com>
2025-04-21 20:01:22 +0200

    Retrived local IP.
    Retrieve local IP programatically.
    
    Co-athured-by: ChatGPT <no-reply.com>

--- deploy.sh
+++ deploy.sh
@@ -4,34 +4,36 @@ docker image pull lukan707/minitwit-joel-web:latest
 docker image pull prom/prometheus:latest
 docker image pull grafana/grafana:10.2.4
 
-# shellcheck disable=SC2046
-docker rm -f $(docker ps -aq)
+ SWARM_IP=$(ip -4 addr show $(ip route show default | awk '/default/ {print $5}') | awk '/inet / {print $2}' | cut -d/ -f1)
 
-docker network create minitwit-network
+# # shellcheck disable=SC2046
+# docker rm -f $(docker ps -aq)
 
-docker container prune -f
+# docker network create minitwit-network
 
-if [ "$1" == "--local" ]; then
-    docker run -d --mount source=minitiwt-devops_db-data,target=/var/lib/postgresql/data --network=minitwit-network -p 5432:5432 --name=minitwit-db --env-file .secrets-local lukan707/minitwit-joel-db:latest
+# docker container prune -f
 
-    until  docker exec minitwit-db pg_isready -U postgres; do
-        sleep 1
-        echo 'Database is not ready'
-    done
+# if [ "$1" == "--local" ]; then
+#     docker run -d --mount source=minitiwt-devops_db-data,target=/var/lib/postgresql/data --network=minitwit-network -p 5432:5432 --name=minitwit-db --env-file .secrets-local lukan707/minitwit-joel-db:latest
 
-    docker run -d --network=minitwit-network -p 8080:8080 -p 8081:8081 --name=minitwit-api --env-file .secrets-local lukan707/minitwit-joel-api:latest
-else
-    docker run -d --network=minitwit-network -p 8080:8080 -p 8081:8081 --name=minitwit-api --env-file .secrets-production lukan707/minitwit-joel-api:latest
-fi
+#     until  docker exec minitwit-db pg_isready -U postgres; do
+#         sleep 1
+#         echo 'Database is not ready'
+#     done
 
-docker run -d --network=minitwit-network -p 3000:3000 --name=minitwit-web -e "API_BASE_URL=http://minitwit-api:8080/" -e "NODE_TLS_REJECT_UNAUTHORIZED=0" lukan707/minitwit-joel-web:latest 
+#     docker run -d --network=minitwit-network -p 8080:8080 -p 8081:8081 --name=minitwit-api --env-file .secrets-local lukan707/minitwit-joel-api:latest
+# else
+#     docker run -d --network=minitwit-network -p 8080:8080 -p 8081:8081 --name=minitwit-api --env-file .secrets-production lukan707/minitwit-joel-api:latest
+# fi
 
-until curl -f http://localhost:8080/api/sim/latest; do
-    sleep 1
-    echo 'Minitwit API is not ready'
-done
+# docker run -d --network=minitwit-network -p 3000:3000 --name=minitwit-web -e "API_BASE_URL=http://minitwit-api:8080/" -e "NODE_TLS_REJECT_UNAUTHORIZED=0" lukan707/minitwit-joel-web:latest 
 
-docker run -d --network=minitwit-network -p 9090:9090 --name=prometheus -v ./prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:latest
-docker run -d --network=minitwit-network -p 3001:3000 --name=grafana -v ./grafana/provisioning/dashboards:/etc/grafana/provisioning/dashboards -v ./grafana/dashboards:/var/lib/grafana/dashboards -v ./grafana/provisioning/datasources:/etc/grafana/provisioning/datasources grafana/grafana:10.2.4
+# until curl -f http://localhost:8080/api/sim/latest; do
+#     sleep 1
+#     echo 'Minitwit API is not ready'
+# done
 
-docker image prune -f 
+# docker run -d --network=minitwit-network -p 9090:9090 --name=prometheus -v ./prometheus/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:latest
+# docker run -d --network=minitwit-network -p 3001:3000 --name=grafana -v ./grafana/provisioning/dashboards:/etc/grafana/provisioning/dashboards -v ./grafana/dashboards:/var/lib/grafana/dashboards -v ./grafana/provisioning/datasources:/etc/grafana/provisioning/datasources grafana/grafana:10.2.4
+
+# docker image prune -f 
